6e227b29bb9dcf118513cba7821eff2274f8d559
Gustaluna <lgsantana.cin@gmail.com>
2023-03-12 23:47:10 -0300

    async await

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,12 +1,19 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro){
-            throw Error("Esse CEP nao existe")
-        }
-        console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('processamento concluido'))
+// let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+//     .then(resposta => resposta.json())
+//     .then(r => {
+//         if(r.erro){
+//             throw Error("Esse CEP nao existe")
+//         }
+//         console.log(r)
+//     })
+//     .catch(erro => console.log(erro))
+//     .finally(mensagem => console.log('processamento concluido'))
 
-console.log(consultaCEP)
+// console.log(consultaCEP)
+
+async function buscaEndereco() {
+    let consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+    let consultaCEPConvertida = await consultaCEP.json()
+    console.log(consultaCEPConvertida)
+}
+buscaEndereco()
