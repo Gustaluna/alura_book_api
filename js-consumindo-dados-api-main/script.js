5bbe7e6bc76252fd0b50ae969605436422f800d7
Gustaluna <lgsantana.cin@gmail.com>
2023-03-12 23:39:10 -0300

    finally

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,5 +1,12 @@
 let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r =>console.log(r));
+    .then(resposta => resposta.json())
+    .then(r => {
+        if(r.erro){
+            throw Error("Esse CEP nao existe")
+        }
+        console.log(r)
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('processamento concluido'))
 
 console.log(consultaCEP)
