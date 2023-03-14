e2d6248a80876195a43b523b0535b012cc050599
Gustaluna <lgsantana.cin@gmail.com>
2023-03-13 22:17:00 -0300

    final

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -12,15 +12,24 @@
 // console.log(consultaCEP)
 
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById("erro")
+    mensagemErro.innerHTML = ""
     try{
         let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         let consultaCEPConvertida = await consultaCEP.json()
         if(consultaCEPConvertida.erro){
             throw Error('CEP nao existente')
         }
+        var cidade = document.getElementById("cidade")
+        var logladouro = document.getElementById("endereco")
+        var estado = document.getElementById("estado")
+        cidade.value = consultaCEPConvertida.localidade
+        logladouro.value =  consultaCEPConvertida.logradouro
+        estado.value =  consultaCEPConvertida.uf
         console.log(consultaCEPConvertida)
         return consultaCEPConvertida
     } catch (erro){
+        mensagemErro.innerHTML = `<p>CEP Invalido. Tente novamente!</p>`
         console.log(erro)
     }  
 }
@@ -28,3 +37,6 @@ async function buscaEndereco(cep) {
 // let ceps = ['01001000','01001001']
 // let conjuntoDeCeps = ceps.map(valores => buscaEndereco(valores))
 // Promise.all(conjuntoDeCeps).then(respostas => console.log(respostas))
+
+var cep = document.getElementById('cep')
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
